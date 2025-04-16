e763196b791c8aeee3a0c77aa3bd41ffbbe520cb
Dayakarreddy00111 <dayakarreddy00111@gmail.com>
2025-04-16 11:26:03 +0530

    this is my 1 commit

---
+++ javacode.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class LoginSystem {
+    public static void main(String[] args) {
+        // Preset username and password
+        String validUsername = "admin";
+        String validPassword = "password123";
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter username: ");
+        String inputUsername = scanner.nextLine();
+
+        System.out.print("Enter password: ");
+        String inputPassword = scanner.nextLine();
+
+        if (inputUsername.equals(validUsername) && inputPassword.equals(validPassword)) {
+            System.out.println("Login successful. Welcome, " + inputUsername + "!");
+        } else {
+            System.out.println("Invalid username or password.");
+        }
+
+        scanner.close();
+    }
+}
